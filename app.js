87636b3ef9de781081bc4bc2aeea3bbfdcf29660
kumarananda <coderanandakumar@gmail.com>
2022-05-28 17:03:59 +0600

    upload

---
+++ app.js
@@ -0,0 +1,140 @@
+const devs = [
+  {
+    id: 1,
+    name: "Ananda",
+    skill: "MERN",
+    age: 30,
+    location: "Panthapath",
+  },
+  {
+    id: 2,
+    name: "Adnan",
+    skill: "IOS devs",
+    age: 50,
+    location: "Mirpur",
+  },
+  {
+    id: 3,
+    name: "Anamul",
+    skill: "MERN",
+    age: 80,
+    location: "Panthapath",
+  },
+  {
+    id: 4,
+    name: "Achem",
+    skill: "MERN",
+    age: 110,
+    location: "Mirpur",
+  },
+  {
+    id: 5,
+    name: "Reiday",
+    skill: "MERN",
+    age: 10,
+    location: "Uttara",
+  },
+];
+
+console.log(
+  devs.some((data) => {
+    return data.age > 120;
+  })
+);
+
+// 07
+// console.log(
+//   devs.some((data) => {
+//     return data.age > 120;
+//   })
+// );
+// false
+
+// 06 some return boolean type data true/false
+// if data found in our selected data with our condition than true
+// otherwise false
+// console.log(
+//   devs.some((data) => {
+//     return data.age < 20;
+//   })
+// );
+// true
+
+// 05
+// console.log(
+//   devs.filter((data) => {
+//     return data.age < 20;
+//   })
+// );
+// [
+//   { id: 5, name: 'Reiday', skill: 'MERN', age: 10, location: 'Uttara' }
+// ]
+
+//04  filter return data in  Array pattrn and related data
+// console.log(
+//   devs.filter((data) => {
+//     return data.location == "Panthapath";
+//   })
+// );
+/**
+ * [
+  {
+    id: 1,
+    name: 'Ananda',       
+    skill: 'MERN',        
+    age: 30,
+    location: 'Panthapath'
+  },
+  {
+    id: 3,
+    name: 'Anamul',       
+    skill: 'MERN',        
+    age: 80,
+    location: 'Panthapath'
+  }
+]
+ */
+
+// find return one data in  ofject pattrn
+// 03 same location data is found in our data but find return only one data
+// console.log(
+//   devs.find((data) => {
+//     return data.location == "Panthapath";
+//   })
+// );
+/**
+ * {
+  id: 1,
+  name: 'Ananda',       
+  skill: 'MERN',        
+  age: 30,
+  location: 'Panthapath'
+}
+ */
+
+//02 find retun selected id data
+/**
+ * {
+  id: 3,
+  name: 'Anamul',       
+  skill: 'MERN',        
+  age: 80,
+  location: 'Panthapath'
+}
+ */
+
+// 01 find only return one data
+// console.log(
+//   devs.find((data) => {
+//     return data;
+//   })
+// );
+/**
+ * {
+  id: 1,
+  name: 'Ananda',
+  skill: 'MERN',
+  age: 30,
+  location: 'Panthapath'
+}
+ */
