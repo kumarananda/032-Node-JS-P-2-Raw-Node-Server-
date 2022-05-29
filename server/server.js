033af6737c9b8e262e460f9391ea8703842c5d94
kumarananda <coderanandakumar@gmail.com>
2022-05-29 18:53:33 +0600

    update

--- server/server.js
+++ server/server.js
@@ -1,28 +1,47 @@
 // import http {createServer} from 'http';
-import fs from 'fs';
+import {readFileSync} from 'fs';
 import dotenv from 'dotenv';
 import { createServer } from 'http';
 
 // environment init
 dotenv.config();
-let student_json = fs.readFileSync('./data/student.json', )
-let student_obj = JSON.parse(student_json)
-
-// let student_obj
 let PORT = process.env.SERVER_PORT;
 
 //data manage
+let student_json = readFileSync('./data/student.json', )
+let student_obj = JSON.parse(student_json) 
+
+
+
+
 
 
  
+ 
 createServer( (req, res) => {
 
-    if(req.url == '/api/')
+    if(req.url == '/api/students' && req.method == 'GET'){
+        res.writeHead('200', {"Content-Type" : 'application/json' });
+        res.end(student_json)
+    }if(req.url.match(/\/api\/students\/[0-9]{1,}/)  && req.method == 'GET'){
 
-    res.writeHead('200', {"Content-Type" : 'application/json' });
+        let urlToArry = req.url.split('/')
+        let urlRevarse = urlToArry.reverse();
+        let id = urlRevarse[0]
 
-    res.end(student_json)
+        
+
+        res.writeHead('200', {"Content-Type" : 'application/json' });
+        res.end(student_json)
+    }else{
+        res.writeHead('200', {"Content-Type" : 'application/json' });
+        res.end(JSON.stringify({
+            error_mes : "Invlide Route"
+        }))
+    }
+
+
 }).listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })    
-console.log(PORT);   
+console.log(PORT);    
