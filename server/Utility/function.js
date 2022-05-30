ee577a073c05e24d293d20c64fcd924b7319570f
kumarananda <coderanandakumar@gmail.com>
2022-05-31 01:21:57 +0600

    edit running

---
+++ server/Utility/function.js
@@ -0,0 +1,4 @@
+// find Last ID And Make New 
+export const findLastID = (obj) => {
+    return (obj[obj.length - 1].id + 1); 
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 // import http {createServer} from 'http';
-import {readFileSync} from 'fs';
+import {readFileSync, writeFileSync} from 'fs';
 import dotenv from 'dotenv';
 import { createServer } from 'http';
+import { findLastID } from './Utility/function.js';
+
 
 // environment init
 dotenv.config();
@@ -52,17 +54,65 @@ createServer( (req, res) => {
         req.on('end', () => {
             let chunkObject = JSON.parse(data)
             // console.log(chunkObject);
-            // student_obj.push({
-            //     id : "",
-            //     name : 
-            // })
+            let {name, age, skill, location, id} = chunkObject;
+            student_obj.push({
+                id : findLastID(student_obj),
+                name : name,
+                age : age,
+                skill: skill,     
+                location : location  
+            })
+            writeFileSync('./data/student.json', JSON.stringify(student_obj));
+            
         });
 
         res.writeHead('200', {"Content-Type" : 'application/json' });
-            res.end(JSON.stringify({
-                message : "Post method is OK"
-            }))
+        res.end(JSON.stringify({
+            message : "Post Update Succussfully"
+        }))
         
+    }else if(req.url.match(/\/api\/students\/[0-9]{1,}/)  && req.method == 'DELETE'){
+
+        // let urlToArry = req.url.split('/'); let urlRevarse = urlToArry.reverse(); let id = urlRevarse[0]
+        let id = req.url.split('/')[3];
+        let update_data = student_obj.filter(stu => stu.id != id);
+
+        if(student_obj.some(stu => stu.id == id)){
+            writeFileSync('./data/student.json', JSON.stringify(update_data) )
+            res.writeHead('200', {"Content-Type" : 'application/json' });
+            res.end(JSON.stringify({
+                mess : 'Student data Deleted succssfully'
+            }));
+        }else{
+            res.writeHead('200', {"Content-Type" : 'application/json' });
+            res.end(JSON.stringify({
+                mess : 'Student data not found'
+            }));
+        }
+
+    }else if( ( req.method == 'PATCH' || req.method == 'PUT') && req.url.match(/\/api\/students\/[0-9]{1,}/) ){
+
+        let id = req.url.split('/')[3];
+
+        
+        if(student_obj.some(stu => stu.id == id)){
+
+            student_obj[student_obj.findIndex(stu => stu.id == id )] = {
+                
+            }
+            // writeFileSync('./data/student.json', JSON.stringify(update_data) )
+            res.writeHead('200', {"Content-Type" : 'application/json' });
+            res.end(JSON.stringify({
+                mess : 'Student data Edited succssfully'
+            }));
+        }else{
+            res.writeHead('200', {"Content-Type" : 'application/json' });
+            res.end(JSON.stringify({
+                mess : 'Student data not edit/found'
+            }));
+        }
+
+
     }else{
         res.writeHead('200', {"Content-Type" : 'application/json' });
         res.end(JSON.stringify({
